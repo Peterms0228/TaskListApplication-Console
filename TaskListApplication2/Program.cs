c5fc215c7577fbe3988462f6455667af41d568ab
peter-030 <chuams-wm19@student.tarc.edu.my>
2023-09-14 14:33:35 +0800

    14/9/2023 14:33 - create main menu - create Task object

--- TaskListApplication2/Program.cs
+++ TaskListApplication2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TaskListApplication2
 {
@@ -6,7 +7,62 @@ namespace TaskListApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Simple Task List Application - Yokogawa");
+
+            //Temporary Memory
+            List<Task> taskList = new List<Task>();
+            string option = "";
+
+            while(option != "q")
+            {
+                Console.WriteLine("Enter 1 to Add Task");
+                Console.WriteLine("Enter 2 to View Tasks");
+                Console.WriteLine("Enter 3 to Update Task");
+                Console.WriteLine("Enter 4 to Delete Task");
+                Console.WriteLine("Enter q to Exit");
+
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        addTask();
+                        break;
+                    case "2":
+                        viewTask();
+                        break;
+                    case "3":
+                        updateTask();
+                        break;
+                    case "4":
+                        deleteTask();
+                        break;
+                    default:
+                        break;
+                }
+            }
+                
+            
+        }
+
+        static void addTask()
+        {
+
+        }
+
+        static void viewTask()
+        {
+
+        }
+
+        static void updateTask()
+        {
+
+        }
+
+        static void deleteTask()
+        {
+
         }
     }
 }
---
+++ TaskListApplication2/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskListApplication2
+{
+    class Task
+    {
+        string name;
+        DateTime dueDate;
+        string status;
+    }
+}
